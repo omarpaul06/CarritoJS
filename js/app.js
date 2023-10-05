499f53078d3b7a2153c773f9c0394ca9e562ddee
omarpaul06 <ccispichitomar@gmail.com>
2023-10-04 22:41:03 -0500

    finalizado

--- js/app.js
+++ js/app.js
@@ -1,60 +1,101 @@
 //VARIABLES
 const listaCursos = document.querySelector('#lista-cursos');
-const carrito = document.querySelector('#lista-carrito tbody')
-let articulosCarrito = [];
+let carrito = [];
+const listaCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 
- eventListener();
+//EVENTOS
+eventListener();
 function eventListener(){
-    listaCursos.addEventListener('click', seleccionarCurso);
-    
+    listaCursos.addEventListener('click', leerDatosCurso);
+    vaciarCarritoBtn.addEventListener('click', ()=> {
+        carrito = [];
+        limpiarHTML();
+    });
+    listaCarrito.addEventListener('click', eliminarCurso);
 }
 
-function seleccionarCurso(e){
+//FUNCIONES
+function leerDatosCurso(e){
     e.preventDefault();
-    
-    if(e.target.classList.contains('agregar-carrito')){
-        const detalle = e.target.parentElement.parentElement;
-        leerDatosCurso(detalle);
-    }
 
+    if(e.target.classList.contains('agregar-carrito')){
+        cursoSeleccionado = e.target.parentElement.parentElement;
+        detalleCurso(cursoSeleccionado);
+    }
+    
 }
 
-function leerDatosCurso(curso){
-    
-    const desCurso = {
+function detalleCurso(curso){
+    const detalleCurso = {
         imagen: curso.querySelector('img').src,
         titulo: curso.querySelector('h4').textContent,
         precio: curso.querySelector('p span').textContent,
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
-        
     }
-    articulosCarrito = [...articulosCarrito, desCurso];
-    
-    carritoHTML();
+
+    //Verifica si selecciona el mismo curso
+    const existe = carrito.some(curso => curso.id === detalleCurso.id);
+    if(existe){
+       //Actualizamos la cantidad
+        const cursos = carrito.map(curso => {
+            if(curso.id === detalleCurso.id){
+                curso.cantidad++;
+                return curso; //Retorna el objeto actualizado
+            }else{
+                return curso; //Retorna los objetos que no son duplicados
+            }   
+        });
+        carrito = [...cursos]; //Cuenta
+    }else{
+        carrito = [...carrito,detalleCurso]
+    }
+
+
    
+    console.log(carrito)
+    carritoHTML();
 }
 
-function carritoHTML() {
+function carritoHTML(){
+
     limpiarHTML();
-
-    articulosCarrito.forEach( curso =>{
+    carrito.forEach(curso =>{
         const row = document.createElement('tr');
-        const {imagen, titulo, precio, cantidad, id} = curso;
+        const {imagen, titulo, precio, id, cantidad} = curso;
         row.innerHTML = `
-        <td> <img src="${imagen}" width="100"> </td>
-        <td> ${titulo} </td>
-        <td> ${precio}</td>
-        <td> ${cantidad}</td>
-        <td> <a href="#" class="borrar-curso" data-id=${id}>X</a></td>
+            <td><img src="${imagen}" width="100"></td>
+            <td>${titulo}</td>
+            <td>${precio}</td>
+            <td>${cantidad}</td>
+            <td><a href="" class="borrar-curso" data-id=${id}>X</a></td>
         `
-
-        carrito.appendChild(row);
+        listaCarrito.appendChild(row);
     })
 }
 
 function limpiarHTML(){
-    while(carrito.firstChild){
-        carrito.removeChild(carrito.firstChild);
+    while(listaCarrito.firstChild){
+        listaCarrito.removeChild(listaCarrito.firstChild)
+    }
+}
+
+function eliminarCurso(e){
+    e.preventDefault();
+
+    if (e.target.classList.contains('borrar-curso')){
+        const cursoId = e.target.getAttribute('data-id');
+        const existe = carrito.some(curso => {
+            if(curso.id === cursoId){
+                if(curso.cantidad > 1){
+                    curso.cantidad--;
+                    carritoHTML();
+                }else{
+                    carrito = carrito.filter(curso => curso.id !== cursoId);
+                    carritoHTML();
+                }
+            }
+        })
     }
 }
